c7cef77a5f9421951c6b61524a52dc9aad2af459
JSantosP <javiersantospaniego@gmail.com>
2015-02-07 22:14:09 +0100

    Added operation examples

---
+++ src/main/scala/meetup/scala/OperationsExamples.scala
@@ -0,0 +1,14 @@
+package meetup.scala
+
+trait OperationsExamples {
+
+	val list = List(1,2,3)
+	val initialValue = "this list contains :"
+
+	val folded = list.foldLeft(initialValue)(
+		(accumulator,number) => accumulator + " " + number)
+
+	println(folded)
+	
+}
+
